7365d56ebc046ccc90b880aed4dddbe88c9666b7
rillout <fedkv@gmail.com>
2022-03-14 23:31:06 +0300

    Add Sem2_Task_2

---
+++ Sem2_Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным
+
+using System;
+
+namespace Sem2_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+
+        {
+            int i, n = 3;
+            int[] sides = new int[n];
+
+
+            Console.WriteLine("Введите длину сторон треугольника через пробел: 'a b c'");
+            string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            for (i = 0; i < (n < str.Length ? n : str.Length); ++i)
+                sides[i] = Convert.ToInt32(str[i]);
+
+            int a = sides[0];
+            int b = sides[1];
+            int c = sides[2];
+
+            if ((a == b) | (a == c) | (b == c)) Console.WriteLine($"Треугольник ({a}, {b}, {c}) равнобедренный");
+            else Console.WriteLine($"Треугольник ({a}, {b}, {c}) не равнобедренный");
+
+
+        }
+
+
+    }
+}
---
+++ Sem2_Task_2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 2. На вход подаются год, номер месяца и день рождения человека, Определить возраст человека на момент 1 февраля 2022 года.
+
+
+using System;
+using System.Globalization;
+
+namespace Sem2_Task_2
+{
+    class Program
+    {
+        static void Main()
+
+        {
+
+            Console.Clear();
+            var cultureInfo = new CultureInfo("en-GB");
+
+
+            Console.WriteLine("Введите дату рождения в формате ДД.ММ.ГГГГ");
+            string BirthDateString = Console.ReadLine();
+
+            try
+            {
+
+                var BirthDate = DateTime.ParseExact(BirthDateString, "dd.MM.yyyy", cultureInfo);
+
+
+                var CurrentDate = new DateTime(2022, 02, 01, 00, 00, 00);
+                var Age = CurrentDate.Year - BirthDate.Year;
+
+
+                if (BirthDate.Date > CurrentDate.AddYears(-Age)) Age--;
+
+                if (Age%10 == 1) Console.WriteLine($"Возраст на 1 февраля 2022 {Age} год");
+                else if ((Age%10 > 1) && (Age%10 < 5)) Console.WriteLine($"Возраст на 1 февраля 2022 {Age} года");
+                else Console.WriteLine($"Возраст на 1 февраля 2022 {Age} лет");
+                
+            }   
+            catch (FormatException)
+            {
+                Console.WriteLine("Дата введена в неверном формате");
+            }
+
+
+
+        
+        }
+
+
+    }
+
+
+}
