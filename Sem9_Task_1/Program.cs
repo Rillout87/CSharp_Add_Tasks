786387639602ea23948472daac8d08770c9c8359
rillout <fedkv@gmail.com>
2022-04-13 21:44:28 +0300

    Add Sem9

---
+++ Sem9_Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1. Дано предложение. Напишите рекурсивный метод, подсчитывающий количество слов в данном предложении.
+// Словом считается последовательность символов без пробелов.
+
+using System;
+//using System.Globalization;
+
+namespace Sem9_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string sentence = "Без труда - не вытянешь и рыбку из пруда.";
+            WordsCount(sentence);
+
+        }
+
+        static void WordsCount(string text, int i = 0, int count = 0)
+        {
+            if (i < text.Length)
+            {
+                if (Convert.ToString(text[i]) != " ") WordsCount(text, ++i, count);
+                else
+                {
+                    count++;
+                    WordsCount(text, ++i, count);
+                }
+            }
+            else Console.WriteLine($"В данном предложении {count + 1} слов");
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
---
+++ Sem9_Task_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 2. Известно, что пароль длиной 3 символа состоит из латинских букв строчного регистра и цифр от 0 до 9.
+// Напишите рекурсивный метод, который перебирает все комбинации паролей. 
+
+
+using System;
+//using System.Globalization;
+
+namespace Sem9_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string passwordElements = "qwertyuiopasdfghjklzxcvbnm0123456789";
+            string password = "s8j";
+
+            HackPassword(password, passwordElements);
+        }
+        static string HackPassword(string pswrd, string passElems, int i = -1)
+        {
+
+            if (i < passElems.Length)
+            {
+                string currentPswrd = HackPassword(pswrd, passElems, ++i) + HackPassword(pswrd, passElems, ++i) + HackPassword(pswrd, passElems, ++i);
+                if (currentPswrd == pswrd)
+                {
+                    Console.WriteLine($"Пароль подобран: {currentPswrd}");
+                    return "";
+                }
+                else return Convert.ToString((passElems[i]));
+
+            }
+            return "";
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
