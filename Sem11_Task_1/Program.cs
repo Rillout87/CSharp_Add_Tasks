2b9f597400af72dfd1ccc2cbbf870fd3f5e558d5
rillout <fedkv@gmail.com>
2022-04-20 23:03:49 +0300

    Add Sem11

---
+++ Sem11_Task_1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 1. Двумерная матрица заполнена натуральными числами. Найти тройку чисел,
+// для которых площадь треугольника со сторонами, определяемыми данной тройкой, будет максимальна.
+
+
+using System;
+
+
+namespace Sem11_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[,] matrix = {{5,12,5,9,7,13,19,1,22,8},{0,12,5,7,18,25,6,15,20,1},{2,29,24,5,16,14,8,3,10,17}};
+
+            double[] simpleArray = new double[3 * 10];
+
+
+            int index = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    simpleArray[index++] = matrix[i, j];
+                }
+            }
+
+            double maxSquare = 0;
+            double currentSquare = 0;
+
+            double pointA, pointB, pointC;
+
+            for (int a = 0; a < simpleArray.Length - 2; a++)
+            {
+
+                for (int b = a + 1; b < simpleArray.Length - 1; b++)
+                {
+
+                    for (int c = a + 2; c < simpleArray.Length; c++)
+                    {
+                        double semiPerim = (simpleArray[a] + simpleArray[b] + simpleArray[c]) / 2;
+                        currentSquare = Math.Sqrt(semiPerim * (semiPerim - simpleArray[a])
+                        * (semiPerim - simpleArray[b]) * (semiPerim - simpleArray[c]));
+                        
+                        if (currentSquare > maxSquare)
+                        {
+                            maxSquare = currentSquare; pointA = simpleArray[a]; pointB = simpleArray[b]; pointC = simpleArray[c];
+                        }
+                    }
+                }
+            }
+            //Console.WriteLine($"{pointA} {pointB} {pointC} {maxSquare}");
+        }
+    }
+}
+
+
+
---
+++ Sem11_Task_2/Program.cs
@@ -0,0 +1,113 @@
+﻿// Задача 2. Шахматный конь стоит на поле с координатами (x1, y1).
+// Сколько ходов ему потребуется сделать, чтобы забрать неподвижную фигуру на поле (x2, y2)?
+
+// ДЕНИС, ЭТО РЕШЕНИЕ СВОРОВАЛ ИЗ ИНЕТРНЕТА. ДЛЯ СЕБЯ - ИЗУЧИТЬ НОВЫЕ ПРИЕМЫ
+
+using System;
+
+
+namespace Sem11_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int res = ChessTask(8, 1, 1, 8, 8);
+
+        }
+            /// <summary>
+        /// задача о двух конях
+        /// </summary>
+        /// <param name="boardSize"> размер доски </param>
+        /// <param name="x1"> координата X активного коня (отсчет от 1) </param>
+        /// <param name="y1"> координата Y активного коня (отсчет от 1) </param>
+        /// <param name="x2"> координата X пассивного коня (отсчет от 1) </param>
+        /// <param name="y2"> координата Y пассивного коня (отсчет от 1) </param>
+        /// <returns> номер хода или -1 в случае невозможности достигнуть коня </returns>
+        static int ChessTask(int boardSize, int x1, int y1, int x2, int y2)
+        {
+            const int END = int.MaxValue;
+ 
+            // преобразуем координаты
+            x1--; y1--;
+            x2--; y2--;
+ 
+            // вспомогательные переменные
+            int k = 1;
+            bool canMove = true;
+            bool finish = false;
+ 
+            // создаем доску
+            int[,] board = new int[boardSize, boardSize];
+ 
+            // функция хода
+            Action<int, int> step = new Action<int, int>((x, y) =>
+            {
+                if (x >= 0 && y >= 0 && x < boardSize && y < boardSize)
+                {
+                    if (board[x, y] == 0)
+                    {
+                        // сюда еще не ходили
+                        board[x, y] = k + 1;
+ 
+                        // запоминаем, что ход был совершен
+                        canMove = true;
+                    }
+                    else if (board[x, y] == END)
+                    {
+                        // конец
+                        finish = true;
+                    }
+                }
+            });
+ 
+            // помещаем коней
+            board[x1, y1] = k;
+            board[x2, y2] = END;
+ 
+            // начинаем ходить
+            while (canMove)
+            {
+                canMove = false;
+ 
+                for (int x = 0; x < boardSize; x++)
+                {
+                    for (int y = 0; y < boardSize; y++)
+                    {
+                        if (board[x, y] == k)
+                        {
+                            // сюда можно попасть конем на [k-1] ходе
+ 
+                            // рассматриваем возможные ходы
+                            step(x - 2, y - 1);
+                            step(x - 1, y - 2);
+                            step(x + 1, y - 2);
+                            step(x + 2, y - 1);
+                            step(x + 2, y + 1);
+                            step(x + 1, y + 2);
+                            step(x - 1, y + 2);
+                            step(x - 2, y + 1);
+                        }
+                    }
+                }
+ 
+                if (finish)
+                {
+                    // конец
+                    return k;
+                }
+ 
+                k++;
+            }
+ 
+            // достичь коня невозможно
+            return -1;
+        }
+        
+    }
+}
+
+
+
+
